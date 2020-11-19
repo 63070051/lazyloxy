f3817fb2f33c1df334d017361b7e8435333642a0
63070051 <73683531+63070051@users.noreply.github.com>
2020-11-19 18:07:12 +0700

    Update delete.php

--- lab13/itf2020/delete.php
+++ lab13/itf2020/delete.php
@@ -1,11 +1,11 @@
 <?php
   $con = mysqli_connect('itf63070051.mysql.database.azure.com', 'kmitl63070051@itf63070051', 'dH4nxosrgh', 'itf63070051') or die("Error");
-  $id = $_GET["id"];
+  $id = $_GET["user"];
   $query = mysqli_query($con, "delete from guestbook where id = ".$id."");
   if($query){
     header("location: index.php");
   }
   else{
-    echo "<script>alert(\"ลบข้อมูลไม่สำเร็จ: ".mysqli_error($conn)."\");</script> <script>window.location.replace(\"index.php\");</script>";
+    echo "<script>alert(\"ลบข้อมูลไม่สำเร็จ\");</script> <script>window.location.replace(\"index.php\");</script>";
   }
 ?>
