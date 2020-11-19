6a63862e00fefda503bb6f100711f83d33270259
63070051 <73683531+63070051@users.noreply.github.com>
2020-11-19 18:23:46 +0700

    Update update.php

--- lab13/itf2020/update.php
+++ lab13/itf2020/update.php
@@ -1,11 +1,13 @@
 <?php
-	$conn = mysqli_connect('itf63070051.mysql.database.azure.com', 'kmitl63070051@itf63070051', 'dH4nxosrgh', 'itf63070051');
+	$con = mysqli_connect('itf63070051.mysql.database.azure.com', 'kmitl63070051@itf63070051', 'dH4nxosrgh', 'itf63070051');
 
 	$name = $_POST['name'];
 	$comment = $_POST['comment'];
 	$link = $_POST['link'];
 	$id = $_POST['id'];
+
 	$query = mysqli_query($con, 'UPDATE guestbook SET name = "'.$name.'", comment = "'.$comment.'", link = "'.$link.'" WHERE id = '.$id.'');
+	
 	if($query){
 		echo "<script>alert(\"แก้ไขข้อมูลสำเร็จ\");</script> <script>window.location.replace(\"index.php\");</script>";
 	  }
